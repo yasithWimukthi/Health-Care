587a3b9d9f8813b0b53952ee4951f494ad7325f5
SiriwardanaT <74727810+SiriwardanaT@users.noreply.github.com>
2021-03-20 21:02:34 +0530

    updated some interfaces

---
+++ app/src/main/java/com/androidmatters/healthcare/SendPrescription.java
@@ -0,0 +1,14 @@
+package com.androidmatters.healthcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SendPrescription extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_prescription);
+    }
+}
---
+++ app/src/main/java/com/androidmatters/healthcare/UpdatePrescriptionInfo.java
@@ -0,0 +1,15 @@
+package com.androidmatters.healthcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpdatePrescriptionInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_prescription_info);
+    }
+
+}
