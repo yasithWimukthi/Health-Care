bf37d5f22f068dd7c707663e318141c9c72d5620
SiriwardanaT <thariharshana123@gmail.com>
2021-04-25 03:07:00 -0700

    sign up created

---
+++ app/src/main/java/com/androidmatters/healthcare/signUpUi.java
@@ -0,0 +1,14 @@
+package com.androidmatters.healthcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class signUpUi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_ui);
+    }
+}
