9135cc17416207af5f55b98f9003f658b3ba5b07
SiriwardanaT <thariharshana123@gmail.com>
2021-04-26 07:08:04 -0700

    image attached successful

--- app/src/main/java/com/androidmatters/healthcare/AddPresInfo.java
+++ app/src/main/java/com/androidmatters/healthcare/AddPresInfo.java
@@ -2,13 +2,30 @@ package com.androidmatters.healthcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class AddPresInfo extends AppCompatActivity {
-
+    ImageView uploadedImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_add_pres_info);
+        uploadedImage = findViewById(R.id.pres_image);
+
+        Intent intent = getIntent();
+        String imgurl = intent.getStringExtra("imgUrl");
+        Picasso.get().load(Uri.parse(imgurl)).centerCrop().resize(400,400).into(uploadedImage);
+
+
     }
 }
