4438d31953c2c9cee7b30cb3aa9d71ae802cdf2d
yasithWimukthi <it19966922@my.sliit.lk>
2021-04-29 09:43:28 +0530

    create patient model class

--- app/src/main/java/com/androidmatters/healthcare/Model/Patient.java
+++ app/src/main/java/com/androidmatters/healthcare/Model/Patient.java
@@ -1,4 +1,52 @@
 package com.androidmatters.healthcare.Model;
 
 public class Patient {
+    private String firstName;
+    private String lastName;
+    private String dob;
+    private String Address;
+    private int age;
+
+    public Patient() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
