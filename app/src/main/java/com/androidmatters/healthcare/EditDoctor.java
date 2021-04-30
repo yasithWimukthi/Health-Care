a8cc864d90c623e7cf477289eeb780e75d3d1e2b
yasithWimukthi <it19966922@my.sliit.lk>
2021-04-30 11:53:05 +0530

    modify edit doctor ui

--- app/src/main/java/com/androidmatters/healthcare/EditDoctor.java
+++ app/src/main/java/com/androidmatters/healthcare/EditDoctor.java
@@ -3,12 +3,26 @@ package com.androidmatters.healthcare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class EditDoctor extends AppCompatActivity {
+
+    private EditText nameEditText;
+    private EditText hospitalEditText;
+    private EditText specializationEditText;
+    private EditText mobileEditText;
+    private Button updateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_doctor2);
+
+        nameEditText = findViewById(R.id.updateDoctorNameEditText);
+        hospitalEditText = findViewById(R.id.updateDoctorHospitalEditText);
+        specializationEditText = findViewById(R.id.updateDoctorSpecializationEditText);
+        mobileEditText = findViewById(R.id.updateDoctorMobileEditText);
+        updateButton = findViewById(R.id.updateDoctorBtn);
     }
 }
