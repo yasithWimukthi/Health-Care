071ce529e954002147e994a70cda260e9edae584
yasithWimukthi <it19966922@my.sliit.lk>
2021-05-05 20:16:00 +0530

    create Appointment modal

---
+++ app/src/main/java/com/androidmatters/healthcare/Model/Appointment.java
@@ -0,0 +1,34 @@
+package com.androidmatters.healthcare.Model;
+
+public class Appointment {
+    private String email;
+    private String date;
+    private int number;
+
+    public Appointment() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
