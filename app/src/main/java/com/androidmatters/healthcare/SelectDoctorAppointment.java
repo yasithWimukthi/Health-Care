1f7434aa59cb6ed62c0b843281496ab1cf247240
Cham-88D <alexeidz81@gmail.com>
2021-05-08 02:35:26 +0530

    change and add commits

--- app/src/main/java/com/androidmatters/healthcare/SelectDoctorAppointment.java
+++ app/src/main/java/com/androidmatters/healthcare/SelectDoctorAppointment.java
@@ -36,6 +36,7 @@ public class SelectDoctorAppointment extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.display_doctor_list);
         Context context;
