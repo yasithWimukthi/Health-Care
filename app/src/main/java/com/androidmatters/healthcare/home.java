4274b2a7117d47b77b57d2328a941cdef0fce606
yasithWimukthi <it19966922@my.sliit.lk>
2021-05-08 10:44:26 +0530

    Modify home screen

--- app/src/main/java/com/androidmatters/healthcare/home.java
+++ app/src/main/java/com/androidmatters/healthcare/home.java
@@ -1,11 +1,16 @@
 package com.androidmatters.healthcare;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -13,6 +18,7 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.androidmatters.healthcare.util.CurrentUser;
 import com.google.firebase.firestore.CollectionReference;
@@ -32,6 +38,32 @@ public class home extends AppCompatActivity {
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private CollectionReference patientCollection = db.collection("patients");
     private CollectionReference doctorCollection = db.collection("doctors");
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        boolean permissionGranted = false;
+        switch(requestCode){
+            case 9:
+                permissionGranted = grantResults[0]== PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+        if(permissionGranted){
+            makeCall();
+        }else {
+            Toast.makeText(getApplicationContext(), "You don't assign permission.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void makeCall() {
+        if (ActivityCompat.checkSelfPermission(home.this,
+                Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+            Intent callIntent = new Intent(Intent.ACTION_CALL);
+            callIntent.setData(Uri.parse("tel:1990"));
+            home.this.startActivity(callIntent);
+        }else{
+            Toast.makeText(getApplicationContext(), "You don't assign permission.", Toast.LENGTH_SHORT).show();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,11 +110,23 @@ public class home extends AppCompatActivity {
         callAmbulance.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent callIntent = new Intent(Intent.ACTION_CALL);
-                callIntent.setData(Uri.parse("tel:"+"1990"));
-                startActivity(callIntent);
+                if (Build.VERSION.SDK_INT < 23) {
+                    makeCall();
+                }else {
+                    if (ActivityCompat.checkSelfPermission(home.this,
+                            Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+
+                        makeCall();
+                    }else {
+                        final String[] PERMISSIONS_STORAGE = {Manifest.permission.CALL_PHONE};
+                        //Asking request Permissions
+                        ActivityCompat.requestPermissions(home.this, PERMISSIONS_STORAGE, 9);
+                    }
+                }
             }
         });
+
+
     }
 
 }
